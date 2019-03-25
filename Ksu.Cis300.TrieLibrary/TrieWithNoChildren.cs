c1c7b2a4250f4dd8979f9d7a7e9a7d16382ec244
petefontaine7 <petefontaine7@gmail.com>
2019-03-25 11:57:33 -0500

    89 46 56 87

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,59 @@
+﻿/* TrieWithNoChildren.cs
+ * Author: Peter Fontaine
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// TrieWithNoChildren interfaces a trie node for the case of no child nodes.
+    /// </summary>
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// This bool tells whether this is the end of a string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// Add adds a string to trie.
+        /// </summary>
+        /// <param name="s"> s is the string to be added. </param>
+        /// <returns> An ITrie is returned. </returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _hasEmptyString);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Contains checks to see if a string is in a trie.
+        /// </summary>
+        /// <param name="s"> s is the string to be checked. </param>
+        /// <returns> A bool is returned to tell if the string is in the trie. </returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,98 @@
+﻿/* TrieWithOneChild.cs
+ * Author: Peter Fontaine
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// TrieWithOneChild interfaces ITrie for the case of of trie node child.
+    /// </summary>
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// This tells whether a trie node is the end of a string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// This contains the child of a trie node.
+        /// </summary>
+        private ITrie _child;
+
+        /// <summary>
+        /// This is the label of the child of a trie node.
+        /// </summary>
+        private char _childLabel;
+
+        /// <summary>
+        /// This is the constructor for a TrieWithOneChild.
+        /// </summary>
+        /// <param name="s"> s is the string to be stored in a trie. </param>
+        /// <param name="b"> b is the emptystring bool to be passed. </param>
+        public TrieWithOneChild(string s, bool b)
+        {
+            if (s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                _hasEmptyString = b;
+                _childLabel = s[0];
+                _child = new TrieWithNoChildren();
+                _child = _child.Add(s.Substring(1));
+            }
+        }
+
+        /// <summary>
+        /// Add takes a string and stores it in a trie.
+        /// </summary>
+        /// <param name="s"> s is the string to be stored. </param>
+        /// <returns> An ITrie is returned. </returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] == _childLabel)
+            {
+                _child = _child.Add(s.Substring(1));
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _hasEmptyString, _childLabel, _child);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Contains checks to see if a string is contained in a trie.
+        /// </summary>
+        /// <param name="s"> s is the string to be checked. </param>
+        /// <returns> A bool is returned to tell if s is in the trie or not. </returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else
+            {
+                if (s[0] != _childLabel)
+                {
+                    return false;
+                }
+                else
+                {
+                    return _child.Contains(s.Substring(1));
+                }
+            }
+        }
+    }
+}
